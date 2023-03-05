0c1f24e6a7dac586c3e20850be587bc0af54cb98
DzmitryHome <isys35@mail.ru>
2023-03-05 18:40:33 +0300

    add lesson 22

---
+++ lesson22/code/4.py
@@ -0,0 +1,6 @@
+@dp.message_handler()
+async def echo(message: types.Message):
+    # old style:
+    # await bot.send_message(message.chat.id, message.text)
+
+    await message.answer(message.text)
